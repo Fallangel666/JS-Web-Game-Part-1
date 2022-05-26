81a02674422430fc65c3b526bf737bb5b7c1d9e2
Fallanagel <havemedance@hotmail.com>
2022-05-25 23:28:23 -0500

    made fuction

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+const newImage =(image)=>{
+    return let; image = document.createElement('img');
+    image.src = 'assets/image.gif';
+    image.style.position = 'fixed';
+    image.style.left = '100px';
+    image.style.bottom = '100px';
+    document.body.append(image);
+}
+
+newImage(greenCharacter)
+
+newImage(pineTree)
+
+let sword = document.createElement('img')
+sword.src = 'assets/sword.png'
+sword.position = 'fixed'
+sword.left = '500px'
+sword.bottom = '405px'
+document.body.append(sword)
+
