a90e40c22c6151affc517e830e20ad4b3cb77fa2
Fallanagel <havemedance@hotmail.com>
2022-05-31 22:01:25 -0500

    fixed

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-const newImage =(image)=>{
-    return let; image = document.createElement('img');
-    image.src = 'assets/image.gif';
-    image.style.position = 'fixed';
-    image.style.left = '100px';
-    image.style.bottom = '100px';
-    document.body.append(image);
+const newImage =(url, left, bottom)=>{
+     let object = document.createElement('img');
+    object.src = url
+    object.style.position = 'fixed';
+    object.style.left = left + 'px';
+    object.style.bottom = bottom + 'px';
+    document.body.append(object);
+    return object
 }
 
-newImage(greenCharacter)
+const newItem =(url, left, bottom)=>{
+    let object = newImage(url. left, bottom)
 
-newImage(pineTree)
+    object.addEventListener('dblclick', () => {
+        object.remove()
+    })
+}
 
-let sword = document.createElement('img')
-sword.src = 'assets/sword.png'
-sword.position = 'fixed'
-sword.left = '500px'
-sword.bottom = '405px'
-document.body.append(sword)
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png' , 450, 200)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
 
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
